91f5e371e65748265822e0d60025fc7a4fad0143
nudoru <kheavy@nudoru.com>
2015-04-06 21:15:53 -0400

    Removing jQuery dependency

--- source/scripts/utils/DOMUtils.js
+++ source/scripts/utils/DOMUtils.js
@@ -1,29 +1,20 @@
-// some from http://youmightnotneedjquery.com/
 var DOMUtils = {
 
   // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
   // element must be entirely on screen
   isElementEntirelyInViewport: function (el) {
-
-    //special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
-      el = el[0];
-    }
-
     var rect = el.getBoundingClientRect();
-
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   },
 
   // element may be partialy on screen
   isElementInViewport: function (el) {
     var rect = el.getBoundingClientRect();
-
     return rect.bottom > 0 &&
       rect.right > 0 &&
       rect.left < (window.innerWidth || document.documentElement.clientWidth)  &&
@@ -41,15 +32,10 @@ var DOMUtils = {
     };
   },
 
+  // from http://jsperf.com/jquery-offset-vs-offsetparent-loop
   offset: function(el) {
-    //var rect = el.getBoundingClientRect();
-    //
-    //return {
-    //  top: rect.top + document.body.scrollTop,
-    //  left: rect.left + document.body.scrollLeft
-    //};
-    // from http://jsperf.com/jquery-offset-vs-offsetparent-loop
-    var ol = ot = 0;
+    var ol = 0,
+        ot = 0;
     if (el.offsetParent) {
       do {
         ol += el.offsetLeft;
@@ -77,6 +63,7 @@ var DOMUtils = {
     return wrapperEl;
   },
 
+  // from youmightnotneedjquery.com
   hasClass: function(el, className) {
     if (el.classList) {
       el.classList.contains(className);
@@ -109,36 +96,4 @@ var DOMUtils = {
     }
   }
 
-  //addClassToElement: function(className, el) {
-  //  if (el.classList) {
-  //    el.classList.add(className);
-  //  } else {
-  //    el.className += ' ' + className;
-  //  }
-  //},
-  //
-  //removeClassFromElement: function(className, el) {
-  //  if (el.classList) {
-  //    el.classList.remove(className);
-  //  } else {
-  //    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-  //  }
-  //},
-  //
-  //toggleClassOnElement: function(className, el) {
-  //  if (el.classList) {
-  //    el.classList.toggle(className);
-  //  } else {
-  //    var classes = el.className.split(' ');
-  //    var existingIndex = classes.indexOf(className);
-  //
-  //    if (existingIndex >= 0) {
-  //      classes.splice(existingIndex, 1);
-  //    } else {
-  //      classes.push(className);
-  //    }
-  //
-  //    el.className = classes.join(' ');
-  //  }
-  //}
 };
