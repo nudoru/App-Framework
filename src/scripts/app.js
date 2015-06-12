4ec4692d61f6dd7b8c820e15f4af4e0e1a22204a
nudoru <kheavy@nudoru.com>
2015-06-12 11:00:22 -0400

    Create appliction to bootstrap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,22 +2,18 @@
 
   var _browserInfo = require('Nudoru.Browser.BrowserInfo');
 
-  if(_browserInfo.notSupported) {
-    alert("Your browser is not supported! Please use IE 9+, Firefox, Chrome or Safari.");
+  if(_browserInfo.notSupported || _browserInfo.isIE) {
+    alert("For the best experience, please use IE10+, Firefox, Chrome or Safari to view this application.");
   }
 
   // Initialize the window
   window.onload = function() {
-
-
 
     // Create the application instance
     window.TT = Nori.createApplication(require('TT.TimeTrackerApplication'));
 
     // Kick off the bootstrapping process
     TT.initialize();
-
-
 
   };
 
