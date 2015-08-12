83130cc37718032391b538675a777c96d0ce38b7
nudoru <kheavy@nudoru.com>
2015-08-12 16:58:31 -0400

    Reducer function example

--- src/scripts/app/model/AppModel.js
+++ src/scripts/app/model/AppModel.js
@@ -2,7 +2,8 @@ define('app/model/AppModel',
   function (require, module, exports) {
 
     var _this,
-        _appEvents  = require('nori/events/EventCreator');
+        _noriEvents         = require('nori/events/EventCreator'),
+        _noriEventConstants = require('nori/events/EventConstants');
 
     //----------------------------------------------------------------------------
     //  Init
@@ -10,7 +11,32 @@ define('app/model/AppModel',
 
     function initialize() {
       _this = this;
-      _appEvents.applicationModelInitialized();
+      Nori.dispatcher().registerReceiver(receiveEvents);
+
+      _noriEvents.applicationModelInitialized();
+    }
+
+    /**
+     * Will receive "firehose" of all events that occur
+     * @param eventObject
+     */
+    function receiveEvents(eventObject) {
+      //console.log('Event occured: ',eventObject);
+      // newState = reducerFunction(getCurrentState(), eventObject);
+    }
+
+    /**
+     * Experimental reducer function based on Redux
+     * https://gaearon.github.io/redux/docs/basics/Reducers.html
+     */
+
+    function reducerFunction(state, event) {
+      switch (event.type) {
+        case _noriEventConstants.MODEL_DATA_CHANGED:
+          return _.assign({}, state, {prop: event.payload.value});
+        default:
+          return state;
+      }
     }
 
     //----------------------------------------------------------------------------
