f8fbaffa71965a36561f7c076e249dfc87116e7c
nudoru <kheavy@nudoru.com>
2015-08-14 10:58:55 -0400

    added cookie module

--- bin/scripts/nori.js
+++ bin/scripts/nori.js
@@ -1,3 +1,40 @@
+define('nori/utils/Cookie',
+  function(require, module, exports) {
+
+    module.exports.create = function(name, value, days) {
+      var expires = "", date;
+      if (days) {
+        date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+    };
+
+    module.exports.read = function(name) {
+      var nameEQ = name + "=",
+        ca = document.cookie.split(';'),
+        i,
+        c;
+      i = ca.length;
+      while (i--) {
+        c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+          return c.substring(nameEQ.length, c.length);
+        }
+      }
+      return null;
+    };
+
+    module.exports.remove = function(name) {
+      module.exports.create(name, '', -1);
+    };
+
+  });
+
 define('nori/utils/Dispatcher',
   function (require, module, exports) {
     var _subjectMap  = {},
