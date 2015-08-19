fab32bbdd41bd4113a9754ea50cd741c5b41d620
nudoru <kheavy@nudoru.com>
2015-08-19 14:27:50 -0400

    Remove logging in rest

--- src/scripts/nori/service/rest.js
+++ src/scripts/nori/service/rest.js
@@ -67,61 +67,71 @@ define('nori/service/Rest',
      * @param success
      * @param error
      */
-    module.exports.Request = function request(reqObj) {
+    var Rest = function () {
 
-      var xhr    = new XMLHttpRequest(),
-          json   = reqObj.json || false,
-          method = reqObj.method.toUpperCase() || 'GET',
-          url    = reqObj.url,
-          data   = reqObj.data || null;
+      function request(reqObj) {
 
-      //return new Promise(function (resolve, reject) {
-      return new Rx.Observable.create(function (observer) {
-        xhr.open(method, url, true, reqObj.user, reqObj.password);
+        var xhr    = new XMLHttpRequest(),
+            json   = reqObj.json || false,
+            method = reqObj.method.toUpperCase() || 'GET',
+            url    = reqObj.url,
+            data   = reqObj.data || null;
 
-        xhr.onreadystatechange = function () {
-          if (xhr.readyState === 4) {
-            if (xhr.status >= 200 && xhr.status < 300) {
-              try {
-                if (json) {
-                  observer.onNext(JSON.parse(xhr.responseText));
-                } else {
-                  observer.onError(xhr.responseText);
+        return new Rx.Observable.create(function makeReq(observer) {
+          xhr.open(method, url, true);
+
+          xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+              if (xhr.status >= 200 && xhr.status < 300) {
+                try {
+                  if (json) {
+                    observer.onNext(JSON.parse(xhr.responseText));
+                  } else {
+                    observer.onError(xhr.responseText);
+                  }
                 }
+                catch (e) {
+                  handleError('Result', 'Error parsing result. Status: ' + xhr.status + ', Response: ' + xhr.response);
+                }
+              } else {
+                handleError(xhr.status, xhr.statusText);
               }
-              catch (e) {
-                handleError('Result', 'Error parsing JSON result');
-              }
-            } else {
-              handleError(xhr.status, xhr.statusText);
             }
+          };
+
+          xhr.onerror   = function () {
+            handleError('Network error');
+          };
+          xhr.ontimeout = function () {
+            handleError('Timeout');
+          };
+          xhr.onabort   = function () {
+            handleError('About');
+          };
+
+          // set non json header? 'application/x-www-form-urlencoded; charset=UTF-8'
+          if (json && method !== "GET") {
+            xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+          } else if (json && method === "GET") {
+            //, text/*
+            xhr.setRequestHeader("Accept", "application/json; odata=verbose");  // odata param for Sharepoint
           }
-        };
 
-        xhr.onerror   = function () {
-          handleError('Network error');
-        };
-        xhr.ontimeout = function () {
-          handleError('Timeout');
-        };
-        xhr.onabort   = function () {
-          handleError('About');
-        };
+          xhr.send(data);
 
-        // set non json header? 'application/x-www-form-urlencoded; charset=UTF-8'
-        if (json && method !== "GET") {
-          xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        } else if (json && method === "GET") {
-          xhr.setRequestHeader("Accept", "application/json, text/*; odata=verbose");  // odata param for Sharepoint
-        }
+          function handleError(type, message) {
+            message = message || '';
+            observer.onError(type + ' ' + message);
+          }
+        });
+      }
 
-        xhr.send(data);
+      return {
+        request: request
+      };
 
-        function handleError(type, message) {
-          message = message || '';
-          observer.onError(type + ' ' + message);
-        }
-      });
     };
+
+    module.exports = Rest();
 
   });
